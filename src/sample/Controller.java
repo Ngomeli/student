fbe32286e11613f3b13a9ef071cefbd5535fa54f
Ngomeli <joshuangomeli@gmail.com>
2020-11-27 13:39:32 +0300

    added the update feature

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,6 +1,5 @@
 package sample;
 
-import javafx.beans.Observable;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -44,14 +43,14 @@ public class Controller implements Initializable {
     private Button btnClear;
     @FXML
     private Button btnExit;
-
+    @FXML
+    private Label statusCheck;
 
     ObservableList<String> majorList = FXCollections.observableArrayList("ACS", "MIS", "LAW");
     SpinnerValueFactory<String> majorFactory = new SpinnerValueFactory.ListSpinnerValueFactory<>(majorList);
 
     ObservableList<String> genderList = FXCollections.observableArrayList("Male", "Female", "Other");
     SpinnerValueFactory<String> genderFactory = new SpinnerValueFactory.ListSpinnerValueFactory<>(genderList);
-
 
 
     @Override
@@ -66,16 +65,45 @@ public class Controller implements Initializable {
         genderFactory.setValue("Specify Gender");
     }
 
-    public Controller() throws SQLException {
-    }
 
-    public void onBtnSearchAction(ActionEvent actionEvent) {
+
+    public void onBtnSearchAction(ActionEvent actionEvent) {/*
+    SELECT * FROM tablename = WHERE ADMNO=?
+    ResultSet rs = search.updateQuery();
+    while(rs.next()){
+
+   */
     }
 
     public void onBtnAddAction(ActionEvent actionEvent) {
+        //INSERT INTO tablename (column....) VALUES(???)
     }
 
     public void onBtnUpdateAction(ActionEvent actionEvent) {
+        myConnection connection = new myConnection();
+        Connection con;
+
+        {
+            try {
+                con = connection.getConnection();
+                PreparedStatement update = con.prepareStatement("update student set sname=?,gender=?,address=?,dob=?,major=?,phoneno=? where admno=?");
+                update.setString(1, txtName.getText());
+                update.setString(2, spGender.getValue());
+                update.setString(3, txtAddress.getText());
+                update.setString(4, String.valueOf(calDOB.getValue()));
+                update.setString(5, spMajor.getValue());
+                update.setString(6, txtPhone.getText());
+                update.setString(7, txtAdmno.getText());
+                update.executeUpdate();
+                if(update.executeUpdate()==0){
+                 statusCheck.setText("Error");
+                }else {
+                    statusCheck.setText("Success");
+                }
+            } catch (Exception d) {
+                System.out.println("Error " + d.getMessage());
+            }
+        }
     }
 
     public void onBtnDeleteAction(ActionEvent actionEvent) throws SQLException {
@@ -86,7 +114,7 @@ public class Controller implements Initializable {
             try {
                 con = connection.getConnection();
                 PreparedStatement delete = con.prepareStatement("delete from student where admno=?");
-                delete.setString(1, "18-1112");
+                delete.setString(1, txtAdmno.getText());
                 delete.execute();
             } catch (Exception d) {
                 System.out.println("Error " + d.getMessage());
